54d3dfba0cb79afcb4662fc0ad1ba50f857af619
Hao Chen <hao.chen@yale.edu>
2023-05-23 14:27:00 -0400

    update

---
+++ src/posix/udp_client.c
@@ -0,0 +1,10 @@
+#include "conn_posix.h"
+#include "lwmavsdk.h"
+#include "target.h"
+
+#include <arpa/inet.h>
+#include <errno.h>
+#include <sys/socket.h>
+#include <unistd.h>
+
+
--- tests/main-battery-fence.c
+++ tests/main-battery-fence.c
@@ -2,37 +2,34 @@
 
 static struct lwm_vehicle_t vehicle;
 
-static enum lwm_action_continuation_t
-callback_on_current_position(struct lwm_action_t* action, struct lwm_action_param_t* param)
+static void
+callback_on_current_position(void * context, mavlink_message_t * msg)
 {
-    ASSERT(action != NULL);
-    ASSERT(param != NULL);
+    ASSERT(msg != NULL);
+    if (msg->msgid != MAVLINK_MSG_ID_GLOBAL_POSITION_INT) {
+        return ;
+    }
 
-    mavlink_message_t* msg = param->detail.msg.msg;
     mavlink_global_position_int_t global_pos;
     mavlink_msg_global_position_int_decode(msg, &global_pos);
     INFO("Current Position: (%f, %f) at %f m\n",
         global_pos.lat / 10000000.0,
         global_pos.lon / 10000000.0,
         global_pos.alt / 1000.0);
-    return LWM_ACTION_CONTINUE;
 }
 
-static enum lwm_action_continuation_t
-    callback_on_battery_status(struct lwm_action_t* action, struct lwm_action_param_t* param)
+static void
+    callback_on_battery_status(void * context, mavlink_message_t * msg)
 {
-    ASSERT(action != NULL);
-    ASSERT(param != NULL);
+    ASSERT(msg != NULL);
 
-    mavlink_message_t* msg = param->detail.msg.msg;
     if (msg->msgid != MAVLINK_MSG_ID_BATTERY_STATUS) {
-        return LWM_ACTION_CONTINUE;
+        return ;
     }
 
     mavlink_battery_status_t battery_status;
     mavlink_msg_battery_status_decode(msg, &battery_status);
     INFO("Battery Status: %d%%\n", battery_status.battery_remaining);
-    return LWM_ACTION_CONTINUE;
 }
 
 int
@@ -50,13 +47,15 @@ main(int argc, char** argv)
         global_pos.lon / 10000000.0,
         global_pos.alt / 1000.0);
 
-    struct lwm_command_t curr_pos;
-    lwm_command_request_message_periodic(&vehicle, &curr_pos,
-        MAVLINK_MSG_ID_GLOBAL_POSITION_INT, 1000000, callback_on_current_position);
+    struct lwm_microservice_t * curr_pos = lwm_microservice_create(&vehicle);
+    curr_pos->handler = callback_on_current_position;
+    curr_pos->context = NULL;
+    lwm_microservice_add_to(&vehicle, MAVLINK_MSG_ID_GLOBAL_POSITION_INT, curr_pos);
 
-    struct lwm_command_t battery;
-    lwm_command_request_message_periodic(&vehicle, &battery,
-        MAVLINK_MSG_ID_BATTERY_STATUS, 1000000, callback_on_battery_status);
+    struct lwm_microservice_t * battery = lwm_microservice_create(&vehicle);
+    battery->handler = callback_on_battery_status;
+    battery->context = NULL;
+    lwm_microservice_add_to(&vehicle, MAVLINK_MSG_ID_BATTERY_STATUS, battery);
 
     lwm_vehicle_spin(&vehicle);
 
