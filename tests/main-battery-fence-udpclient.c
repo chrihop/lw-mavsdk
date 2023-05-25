4229427e244c4bf8b51a57793bd1cd39b3b332ad
Hao Chen <hao.chen@yale.edu>
2023-05-25 11:33:26 -0400

    update

---
+++ tests/main-battery-fence-udpclient.c
@@ -0,0 +1,26 @@
+#include "lwmavsdk.h"
+
+static struct lwm_vehicle_t vehicle;
+
+/**
+ * @brief Battery fence
+ * @details see battery-fence-common.c
+ * @param vehicle
+ */
+extern void battery_fence(struct lwm_vehicle_t* vehicle);
+
+int
+main(int argc, char** argv)
+{
+    enum lwm_error_t err;
+    lwm_vehicle_init(&vehicle);
+    err = lwm_conn_open(&vehicle.conn, LWM_CONN_TYPE_UDP, 14550);
+    if (err != LWM_OK)
+    {
+        PANIC("Failed to open connection\n");
+    }
+
+    battery_fence(&vehicle);
+
+    return 0;
+}
